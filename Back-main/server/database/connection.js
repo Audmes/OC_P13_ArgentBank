0b9701cea6d6b4166a0232a11d00895cdab318d7
Audmes <102157665+Audmes@users.noreply.github.com>
2024-08-28 17:24:13 +0200

    Commit : 28/08/2024 - Modification connection BD

--- Back-main/server/database/connection.js
+++ Back-main/server/database/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+// const databaseUrl =
+//   process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
     console.log('Database successfully connected')
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`)
